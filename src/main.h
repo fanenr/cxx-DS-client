11f1225163798005f03a9c257fe1bb19b9f06db6
Arthur63 <arthur63@163.com>
2024-03-07 19:56:50 +0800

    add Reg

--- src/main.h
+++ src/main.h
@@ -3,21 +3,38 @@
 
 #include "ui_main.h"
 #include "ui_reg.h"
+#include "util.h"
+#include <utility>
+
 #include <QButtonGroup>
 #include <QMessageBox>
 
-class Sreg : public QMainWindow
+#include <QDebug>
+#include <QJsonDocument>
+#include <QJsonObject>
+
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+
+class Reg : public QMainWindow
 {
+  friend class Main;
+
 private:
   Q_OBJECT
-  Ui::Reg *ui;
+  Ui::Reg *ui = new Ui::Reg;
+  QNetworkAccessManager *nam = nullptr;
+
+  int type;
+  QString user;
+  QString pass;
 
 public:
-  Sreg (QWidget *parent = nullptr);
-  ~Sreg () { delete ui; }
+  Reg (QWidget *parent) : QMainWindow (parent) { ui->setupUi (this); };
+  ~Reg () { delete ui; }
 
-public:
-  void reg (QString const &user, QString const &pass);
+private:
+  void req_finished (QNetworkReply *reply);
 
 private slots:
   void
@@ -25,35 +42,82 @@ private slots:
   {
     close ();
   }
+
+  void on_pbtn2_clicked ();
 };
 
-class Mreg : public QMainWindow
+inline void
+Reg::on_pbtn2_clicked ()
 {
-private:
-  Q_OBJECT
-  Ui::Reg *ui;
+  if (!nam)
+    nam = new QNetworkAccessManager (this);
 
-public:
-  Mreg (QWidget *parent = nullptr);
-  ~Mreg () { delete ui; }
+  auto str1 = ui->ledit1->text ();
+  auto str2 = ui->ledit2->text ();
+  auto str3 = ui->ledit3->text ();
 
-public:
-  void reg (QString const &user, QString const &pass);
+  if (str1.isEmpty () || str2.isEmpty () || str3.isEmpty ())
+    {
+      QMessageBox::warning (this, tr ("提示"), tr ("请完整填写信息"));
+      return;
+    }
 
-private slots:
-  void
-  on_pbtn1_clicked ()
-  {
-    close ();
-  }
-};
+  QUrl req_url;
+  QJsonObject req_data;
+  req_data["user"] = user;
+  req_data["pass"] = pass;
+
+  switch (type)
+    {
+    case 1:
+      req_data["id"] = str1;
+      req_data["name"] = str2;
+      req_data["number"] = str3;
+      req_url = URL_STUDENT_NEW;
+      break;
+    case 2:
+      req_data["name"] = str1;
+      req_data["number"] = str2;
+      req_data["position"] = str3;
+      req_url = URL_MERCHANT_NEW;
+      break;
+    default:
+      return;
+    }
+
+  auto req = QNetworkRequest (req_url);
+  req.setHeader (QNetworkRequest::ContentTypeHeader, "application/json");
+  auto req_json = QJsonDocument (req_data).toJson (QJsonDocument::Compact);
+
+  connect (nam, &QNetworkAccessManager::finished, this, &Reg::req_finished);
+  nam->post (req, req_json);
+}
+
+inline void
+Reg::req_finished (QNetworkReply *reply)
+{
+  if (reply->error ())
+    {
+      QMessageBox::warning (this, tr ("提示"), tr ("请求失败"));
+      return;
+    }
+
+  auto res = QJsonDocument::fromJson (reply->readAll ()).object ();
+  if (res["code"] != 0)
+    QMessageBox::warning (this, tr ("注册失败"),
+                          res["data"].toString (tr ("信息丢失")));
+
+  this->close ();
+  QMessageBox::information (nullptr, tr ("提示"), tr ("注册成功"));
+}
 
 class Main : public QMainWindow
 {
 private:
   Q_OBJECT
-  Ui::Main *ui;
-  QButtonGroup *btns;
+  Reg *reg = nullptr;
+  Ui::Main *ui = new Ui::Main;
+  QButtonGroup *btns = nullptr;
 
 public:
   explicit Main (QWidget *parent = nullptr);
@@ -67,51 +131,13 @@ private slots:
   void on_pbtn2_clicked ();
 };
 
-inline Main::Main (QWidget *parent) : QMainWindow (parent), ui (new Ui::Main)
+inline Main::Main (QWidget *parent) : QMainWindow (parent)
 {
   ui->setupUi (this);
 
   btns = new QButtonGroup (this);
   btns->addButton (ui->rbtn1, 1);
   btns->addButton (ui->rbtn2, 2);
-}
-
-inline Sreg::Sreg (QWidget *parent) : ui (new Ui::Reg)
-{
-  ui->setupUi (this);
-
-  setWindowTitle (tr ("学生注册"));
-  ui->label1->setText (tr ("学生注册"));
-  ui->label2->setText (tr ("学号"));
-  ui->label3->setText (tr ("昵称"));
-  ui->label4->setText (tr ("电话"));
-}
-
-inline void
-Sreg::reg (QString const &user, QString const &pass)
-{
-  if (isVisible ())
-    return;
-  show ();
-}
-
-inline Mreg::Mreg (QWidget *parent) : ui (new Ui::Reg)
-{
-  ui->setupUi (this);
-
-  setWindowTitle (tr ("商户注册"));
-  ui->label1->setText (tr ("商户注册"));
-  ui->label2->setText (tr ("名称"));
-  ui->label3->setText (tr ("电话"));
-  ui->label4->setText (tr ("位置"));
-}
-
-inline void
-Mreg::reg (QString const &user, QString const &pass)
-{
-  if (isVisible ())
-    return;
-  show ();
 }
 
 inline int
@@ -127,11 +153,12 @@ Main::category ()
 inline void
 Main::on_pbtn1_clicked ()
 {
-  static Sreg sreg;
-  static Mreg mreg;
+  if (!reg)
+    reg = new Reg (this);
 
-  auto const &user = ui->ledit1->text ();
-  auto const &pass = ui->ledit2->text ();
+  auto type = category ();
+  auto user = ui->ledit1->text ();
+  auto pass = ui->ledit2->text ();
 
   if (user.isEmpty () || pass.isEmpty ())
     {
@@ -139,17 +166,33 @@ Main::on_pbtn1_clicked ()
       return;
     }
 
-  switch (category ())
+  if (reg->isVisible ())
+    return;
+
+  switch (type)
     {
     case 1:
-      sreg.reg (user, pass);
+      reg->setWindowTitle (tr ("学生注册"));
+      reg->ui->label1->setText (tr ("学生注册"));
+      reg->ui->label2->setText (tr ("学号"));
+      reg->ui->label3->setText (tr ("昵称"));
+      reg->ui->label4->setText (tr ("电话"));
       break;
     case 2:
-      mreg.reg (user, pass);
+      reg->setWindowTitle (tr ("商户注册"));
+      reg->ui->label1->setText (tr ("商户注册"));
+      reg->ui->label2->setText (tr ("名称"));
+      reg->ui->label3->setText (tr ("电话"));
+      reg->ui->label4->setText (tr ("位置"));
       break;
     default:
-      break;
+      return;
     }
+
+  reg->user = std::move (user);
+  reg->pass = std::move (pass);
+  reg->type = type;
+  reg->show ();
 }
 
 inline void
---
+++ src/util.h
@@ -0,0 +1,9 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#define URL_BASE "http://127.0.0.1:8000/api"
+
+#define URL_STUDENT_NEW URL_BASE "/student/new"
+#define URL_MERCHANT_NEW URL_BASE "/merchant/new"
+
+#endif
