12496000bd3a0e100325bf7f11e01b7d47880892
Arthur63 <Arthur63@163.com>
2024-03-07 19:56:50 +0800

    add Reg

--- xmake.lua
+++ xmake.lua
@@ -4,6 +4,8 @@ set_languages("cxx17")
 target("client")
     add_rules("qt.widgetapp")
 
+    add_links("Qt6Network")
+    
     add_files("src/*.ui")
     add_files("src/*.cc")
     add_files("src/*.h")
