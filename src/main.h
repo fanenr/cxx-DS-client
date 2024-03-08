f60f71eabf111f9fdcd87a2a0fb93e1037ec55f3
Arthur63 <arthur63@163.com>
2024-03-08 15:02:39 +0800

    tune log and reg

--- src/main.h
+++ src/main.h
@@ -4,7 +4,6 @@
 #include "ui_main.h"
 #include "ui_reg.h"
 #include "util.h"
-#include <utility>
 
 #include <QButtonGroup>
 #include <QMessageBox>
@@ -22,17 +21,15 @@ class Reg : public QMainWindow
 
 private:
   Q_OBJECT
-  Ui::Reg *ui = new Ui::Reg;
-
   QNetworkRequest req;
-  QNetworkAccessManager *nam = nullptr;
+  Ui::Reg *ui = new Ui::Reg;
 
   int type;
   QString user;
   QString pass;
 
 public:
-  Reg (QWidget *parent);
+  Reg (QWidget *parent, int category);
   ~Reg () { delete ui; }
 
 private:
@@ -48,13 +45,30 @@ private slots:
   void on_pbtn2_clicked ();
 };
 
-inline Reg::Reg (QWidget *parent) : QMainWindow (parent)
+inline Reg::Reg (QWidget *parent, int category)
+    : QMainWindow (parent), type (category)
 {
   ui->setupUi (this);
 
-  nam = new QNetworkAccessManager (this);
+  switch (type)
+    {
+    case 1:
+      setWindowTitle (tr ("学生注册"));
+      ui->label1->setText (tr ("学生注册"));
+      ui->label2->setText (tr ("学号"));
+      ui->label3->setText (tr ("昵称"));
+      ui->label4->setText (tr ("电话"));
+      break;
+    case 2:
+      setWindowTitle (tr ("商户注册"));
+      ui->label1->setText (tr ("商户注册"));
+      ui->label2->setText (tr ("名称"));
+      ui->label3->setText (tr ("电话"));
+      ui->label4->setText (tr ("位置"));
+      break;
+    }
+
   req.setHeader (QNetworkRequest::ContentTypeHeader, "application/json");
-  connect (nam, &QNetworkAccessManager::finished, this, &Reg::req_finished);
 };
 
 inline void
@@ -70,7 +84,6 @@ Reg::on_pbtn2_clicked ()
       return;
     }
 
-  QUrl req_url;
   QJsonObject req_data;
   req_data["user"] = user;
   req_data["pass"] = pass;
@@ -81,19 +94,26 @@ Reg::on_pbtn2_clicked ()
       req_data["id"] = str1;
       req_data["name"] = str2;
       req_data["number"] = str3;
-      req_url = URL_STUDENT_NEW;
+      req.setUrl (QUrl (URL_STUDENT_NEW));
       break;
     case 2:
       req_data["name"] = str1;
       req_data["number"] = str2;
       req_data["position"] = str3;
-      req_url = URL_MERCHANT_NEW;
+      req.setUrl (QUrl (URL_MERCHANT_NEW));
       break;
     default:
       return;
     }
 
-  req.setUrl (req_url);
+  static QNetworkAccessManager *nam;
+  if (!nam)
+    {
+      nam = new QNetworkAccessManager (this);
+      connect (nam, &QNetworkAccessManager::finished, this,
+               &Reg::req_finished);
+    }
+
   auto req_json = QJsonDocument (req_data).toJson (QJsonDocument::Compact);
   nam->post (req, req_json);
 }
@@ -123,12 +143,11 @@ class Main : public QMainWindow
 {
 private:
   Q_OBJECT
-  Reg *reg = nullptr;
+  Reg *sreg = nullptr;
+  Reg *mreg = nullptr;
+  QNetworkRequest req;
   Ui::Main *ui = new Ui::Main;
   QButtonGroup *btns = nullptr;
-
-  QNetworkRequest req;
-  QNetworkAccessManager *nam = nullptr;
 
 public:
   explicit Main (QWidget *parent = nullptr);
@@ -144,7 +163,7 @@ private:
       return 1;
     if (ui->rbtn2->isChecked ())
       return 2;
-    return 0;
+    abort ();
   }
 
 private slots:
@@ -160,17 +179,12 @@ inline Main::Main (QWidget *parent) : QMainWindow (parent)
   btns->addButton (ui->rbtn1, 1);
   btns->addButton (ui->rbtn2, 2);
 
-  nam = new QNetworkAccessManager (this);
   req.setHeader (QNetworkRequest::ContentTypeHeader, "application/json");
-  connect (nam, &QNetworkAccessManager::finished, this, &Main::req_finished);
 }
 
 inline void
 Main::on_pbtn1_clicked ()
 {
-  if (!reg)
-    reg = new Reg (this);
-
   auto type = category ();
   auto user = ui->ledit1->text ();
   auto pass = ui->ledit2->text ();
@@ -181,32 +195,26 @@ Main::on_pbtn1_clicked ()
       return;
     }
 
-  if (reg->isVisible ())
-    return;
-
+  Reg *reg;
   switch (type)
     {
     case 1:
-      reg->setWindowTitle (tr ("学生注册"));
-      reg->ui->label1->setText (tr ("学生注册"));
-      reg->ui->label2->setText (tr ("学号"));
-      reg->ui->label3->setText (tr ("昵称"));
-      reg->ui->label4->setText (tr ("电话"));
+      if (!sreg)
+        sreg = new Reg (this, 1);
+      reg = sreg;
       break;
     case 2:
-      reg->setWindowTitle (tr ("商户注册"));
-      reg->ui->label1->setText (tr ("商户注册"));
-      reg->ui->label2->setText (tr ("名称"));
-      reg->ui->label3->setText (tr ("电话"));
-      reg->ui->label4->setText (tr ("位置"));
+      if (!mreg)
+        mreg = new Reg (this, 2);
+      reg = mreg;
       break;
-    default:
-      return;
     }
+
+  if (reg->isVisible ())
+    return;
 
   reg->user = std::move (user);
   reg->pass = std::move (pass);
-  reg->type = type;
   reg->show ();
 }
 
@@ -222,24 +230,29 @@ Main::on_pbtn2_clicked ()
       return;
     }
 
-  QUrl req_url;
   QJsonObject req_data;
   req_data["user"] = user;
   req_data["pass"] = pass;
+  qInfo () << req_data;
 
   switch (category ())
     {
     case 1:
-      req_url = URL_STUDENT_LOG;
+      req.setUrl (QUrl (URL_STUDENT_LOG));
       break;
     case 2:
-      req_url = URL_MERCHANT_LOG;
+      req.setUrl (QUrl (URL_MERCHANT_LOG));
       break;
-    default:
-      return;
     }
 
-  req.setUrl (req_url);
+  static QNetworkAccessManager *nam;
+  if (!nam)
+    {
+      nam = new QNetworkAccessManager (this);
+      connect (nam, &QNetworkAccessManager::finished, this,
+               &Main::req_finished);
+    }
+
   auto req_json = QJsonDocument (req_data).toJson (QJsonDocument::Compact);
   nam->post (req, req_json);
 }
--- xmake.lua
+++ xmake.lua
@@ -3,9 +3,10 @@ set_languages("cxx17")
 
 target("client")
     add_rules("qt.widgetapp")
-
     add_links("Qt6Network")
     
     add_files("src/*.ui")
     add_files("src/*.cc")
     add_files("src/*.h")
+
+    add_defines("QT_DEBUG")
