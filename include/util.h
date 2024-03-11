578366022285e22bc7da54c122d912babcf86cf0
Arthur63 <Arthur63@163.com>
2024-03-11 21:23:23 +0800

    add menu del

--- include/util.h
+++ include/util.h
@@ -19,7 +19,7 @@
 #define URL_MENU_LIST URL_BASE_MENU "/list"
 #define URL_MENU_NEW URL_BASE_MENU "/new"
 #define URL_MENU_MOD URL_BASE_MENU "/mod"
-#define URL_MENU_EVA URL_BASE_MENU "/eva"
+#define URL_MENU_DEL URL_BASE_MENU "/del"
 
 enum class type
 {
