fb513a0f2bd3d2e050a6336fe07d63bd931f7ed5
Arthur63 <Arthur63@163.com>
2024-03-11 20:27:31 +0800

    fix ditem

--- src/ditem.cc
+++ src/ditem.cc
@@ -10,7 +10,7 @@ void
 Ditem::set_info (Dish const &info)
 {
   ui->label1->setText (info.name);
-  ui->label3->setText (info.user);
+  ui->label3->setText (info.uname);
   ui->label4->setText (info.position);
   ui->label2->setText (QString::number (info.price, 'f', 1) + tr (" 元"));
 }
