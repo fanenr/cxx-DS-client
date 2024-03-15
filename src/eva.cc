9ab047ce1b5b0a83681c76fd8ab167a86afe5217
Arthur63 <arthur63@163.com>
2024-03-15 13:02:46 +0800

    add eva mod and del

--- src/eva.cc
+++ src/eva.cc
@@ -45,6 +45,26 @@ Eva::on_pbtn1_clicked ()
 }
 
 void
+Eva::on_pbtn2_clicked ()
+{
+  QJsonObject req_data;
+  req_data["user"] = prnt->info["user"];
+  req_data["pass"] = prnt->info["pass"];
+  req_data["id"] = id;
+
+  QString req_url = URL_EVA_DEL;
+
+  Http http;
+  auto reply = http.post (req_url, req_data);
+  if (!Http::get_data (reply, this).has_value ())
+    return;
+
+  QMessageBox::information (this, tr ("提示"), tr ("操作成功，返回查看"));
+  prnt->load_dish ();
+  close ();
+}
+
+void
 Eva::on_pbtn3_clicked ()
 {
   auto grade = ui->spin->value ();
